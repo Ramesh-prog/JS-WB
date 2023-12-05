067c525438bd7f8d366e1007ead7d691d1b50d42
Ramesh Chandra Mohapatra <77424241+Ramesh-prog@users.noreply.github.com>
2023-12-05 11:18:35 +0000

    some changes on this files

--- 01_basics/02-dataTypes.js
+++ 01_basics/02-dataTypes.js
@@ -1,8 +1,41 @@
-// alert(3 + 3) // we are using nodejs, not browser. 
+//  Primitive
 
-let name = "Ramesh"
-let age = 10
-let isLoggedIn = false 
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
 
 /*
 -> to be able to operate on variables, it is important to know something about the types of data is called datatypes.
